f94ff8957c7886a3c629ebc0d343ac0ad60f5bf6
Diego-Pimenta <diegopimenta888@gmail.com>
2024-11-01 18:50:58 -0300

    feat: creational - builder

---
+++ creational/builder/src/main/java/com/cdp/entity/Address.java
@@ -0,0 +1,61 @@
+package com.cdp.entity;
+
+public class Address {
+
+    private String street;
+    private String houseNumber;
+    private String city;
+    private String zipcode;
+    private String state;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "houseNumber='" + houseNumber + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
